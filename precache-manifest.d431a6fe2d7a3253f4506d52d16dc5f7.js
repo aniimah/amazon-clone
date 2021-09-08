86452f9ede259d3517c50eedc4e729d584de274e
aniimah <aniimakusi@gmail.com>
2021-09-08 22:27:19 +0000

    Updates

---
+++ precache-manifest.d431a6fe2d7a3253f4506d52d16dc5f7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b52b6e6241fdeadcf3852eb1dae298",
+    "url": "/ecommerce-clone/index.html"
+  },
+  {
+    "revision": "ea4913d866349ca4a206",
+    "url": "/ecommerce-clone/static/css/main.aaa16c96.chunk.css"
+  },
+  {
+    "revision": "2e291a69e10ea32e5e33",
+    "url": "/ecommerce-clone/static/js/2.29be5d07.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/ecommerce-clone/static/js/2.29be5d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4913d866349ca4a206",
+    "url": "/ecommerce-clone/static/js/main.686d119b.chunk.js"
+  },
+  {
+    "revision": "6260de05caff57febe6a",
+    "url": "/ecommerce-clone/static/js/runtime-main.ecb299d1.js"
+  },
+  {
+    "revision": "b80953d2d53bf46acf82aa36919f9a51",
+    "url": "/ecommerce-clone/static/media/RANGO.b80953d2.png"
+  },
+  {
+    "revision": "0e4ceae7980c8b00c6ddeb976f305563",
+    "url": "/ecommerce-clone/static/media/eye-shadow.0e4ceae7.png"
+  },
+  {
+    "revision": "40d45dd5cea6b0ba166ec1c190ec7d47",
+    "url": "/ecommerce-clone/static/media/lipgloss.40d45dd5.png"
+  },
+  {
+    "revision": "be3e430a639ddc2816f89094090a3144",
+    "url": "/ecommerce-clone/static/media/makeupbrush.be3e430a.png"
+  },
+  {
+    "revision": "852e02106d77f39dd08d2bb1d1e6647a",
+    "url": "/ecommerce-clone/static/media/makeupkit.852e0210.png"
+  },
+  {
+    "revision": "c55e9c855166cdf5453c972b958dd9b6",
+    "url": "/ecommerce-clone/static/media/model.c55e9c85.jpg"
+  },
+  {
+    "revision": "ed9d8f0ce1ccfe75686c733058522b41",
+    "url": "/ecommerce-clone/static/media/nailpolish.ed9d8f0c.png"
+  },
+  {
+    "revision": "343741e1af6804304d17f45c3019ad56",
+    "url": "/ecommerce-clone/static/media/nikead.343741e1.jpg"
+  },
+  {
+    "revision": "fe29f47853607d3819266a608029dc30",
+    "url": "/ecommerce-clone/static/media/ring light.fe29f478.png"
+  }
+]);
